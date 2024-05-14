e308cb2273f9868346adbe610284b523e733aa9f
Guitar <kittipong.sataya@gmail.com>
2024-05-14 14:05:10 +0700

    submit

---
+++ script.js
@@ -0,0 +1,29 @@
+//Exercise 1.1
+function addNumbers(a, b) {
+    return a + b
+}
+console.log (addNumbers (5,2))
+
+function subtractNumbers(a, b) {
+    return a - b
+}
+console.log (subtractNumbers (5,2))
+
+function miltiplyNumbers(a, b) {
+    return a * b
+}
+console.log (miltiplyNumbers (5,2))
+
+function divideNumbers(a, b) {
+    return a / b
+}
+console.log (divideNumbers (5,2))
+
+//Exercise 1.2
+
+function info (firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy "+hobby);
+}
+info('Kittipong', 'Bangkok', 'play game');
+
+
